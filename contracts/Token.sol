629db1af635da63c0384dd61f3ffd5e2620e6f5f
宋秉徽 <451000936@qq.com>
2025-01-23 16:25:22 +0800

    feat: lint

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import {ERC721} from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract Coin is ERC721 {
+  constructor() ERC721('Coin', 'COIN') {}
+
+  function mint(address to, uint256 tokenId) external {
+    _mint(to, tokenId);
+  }
+}
